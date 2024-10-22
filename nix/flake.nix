955a989e9d90d50945e462d248d1a3a7a662e7cf
Judah Fuller <judahfuller@icloud.com>
2024-10-22 20:07:53 +0100

    Continue home-manager setup

--- nix/flake.nix
+++ nix/flake.nix
@@ -2,13 +2,19 @@
     description = "Macbook Air flake";
 
     inputs = {
+        home-manager = {
+            url = "github:nix-community/home-manager";
+            inputs.nixpkgs.follows = "nixpkgs";
+        };
         nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-        nix-darwin.url = "github:LnL7/nix-darwin";
-        nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
+        nix-darwin = {
+            url = "github:LnL7/nix-darwin";
+            inputs.nixpkgs.follows = "nixpkgs";
+        };
         nix-homebrew.url = "github:zhaofengli-wip/nix-homebrew";
     };
 
-    outputs = inputs@{ self, nixpkgs, nix-darwin, nix-homebrew, ... }:
+    outputs = inputs@{ self, nixpkgs, nix-darwin, nix-homebrew, home-manager, ... }:
     {
         # Build darwin flake using:
         # $ darwin-rebuild build --flake .#simple
@@ -30,6 +36,7 @@
                 ./modules/macos/design.nix
                 ./modules/macos/production.nix
                 ./modules/macos/music.nix
+                ./modules/home/home.nix
             ];
         };
         # Expose the package set, including overlays, for convenience.
