a3f3b73aba2571be43e96f46f83d5e11f86efd65
Judah Fuller <judahfuller@icloud.com>
2024-10-22 22:45:19 +0100

    Setup Sketchybar layout

---
+++ sketchybar/items/clock.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+sketchybar --add item clock right \
+           --set clock update_freq=10 icon=󰃭  script="$PLUGIN_DIR/clock.sh" \
---
+++ sketchybar/items/volume.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+sketchybar --add item volume right \
+           --set volume script="$PLUGIN_DIR/volume.sh" \
+           --subscribe volume volume_change \
