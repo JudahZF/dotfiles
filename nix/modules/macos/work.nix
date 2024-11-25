b069cd331eb3b61aef6d5e7d6a1225fa88561ca9
Judah Fuller <judahfuller@icloud.com>
2024-11-25 12:52:07 +0000

    Prep for new mac

---
+++ nix/modules/macos/work.nix
@@ -0,0 +1,12 @@
+{
+    ...
+}: {
+    homebrew = {
+        casks = [
+            "8x8-work"
+        ];
+        masApps = {
+            "Sonicwall Mobile Connect" = 822514576;
+        };
+    };
+}
