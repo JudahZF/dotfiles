6a33834bf74c59d0988beb02c6511f7c756b9d41
Judah Fuller <judahfuller@icloud.com>
2025-03-27 12:30:41 +0000

    Attempt to fix trouble, install pdf generator

--- nvim/init.lua
+++ nvim/init.lua
@@ -2,6 +2,9 @@ require("config.remaps")
 
 require("config.lazy")
 
+-- Enable line numbers
+vim.opt.number = true
+
 -- Force transparency
 vim.api.nvim_set_hl(0, "Normal", { bg = "NONE" })
 vim.api.nvim_set_hl(0, "NormalFloat", { bg = "NONE" })
