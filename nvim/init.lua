b06a0d5b0d8c4b7a6083927282e435e71806c9fe
Judah Fuller <judahfuller@icloud.com>
2025-03-27 10:11:58 +0000

    Fix transparency

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,3 +1,7 @@
 require("config.leader")
 
 require("config.lazy")
+
+-- Force transparency
+vim.api.nvim_set_hl(0, "Normal", { bg = "NONE" })
+vim.api.nvim_set_hl(0, "NormalFloat", { bg = "NONE" })
--- nvim/lua/plugins/theme.lua
+++ nvim/lua/plugins/theme.lua
@@ -1,13 +1,12 @@
 return {
 	"folke/tokyonight.nvim",
 	lazy = false,
-	opts = {
-		style = "night",
-		transparent = true,
-	},
 	priority = 1000,
 	config = function()
-		require("tokyonight").setup(opts)
-		vim.cmd([[colorscheme tokyonight-night]])
+		require("tokyonight").setup({
+			style = "night",
+			transparent = true,
+		})
+		vim.cmd([[colorscheme tokyonight]])
 	end,
 }
