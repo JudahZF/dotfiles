d22ac21009f09e81ae7b3250ae54811e2622fb50
Judah Fuller <judahfuller@icloud.com>
2025-03-27 10:39:33 +0000

    Add keymaps and trouble

--- nvim/lua/plugins/trouble.lua
+++ nvim/lua/plugins/trouble.lua
@@ -1,5 +1,14 @@
 return {
-  "folke/trouble.nvim",
-  opts = {}, -- for default options, refer to the configuration section for custom setup.
-  cmd = "Trouble",
+	"folke/trouble.nvim",
+	opts = {}, -- for default options, refer to the configuration section for custom setup.
+	cmd = "Trouble",
+	config = function()
+		require("trouble").setup({})
+		builtin = require("trouble")
+		vim.keymap.set("n", "<leader>tt", builtin.toggle, {})
+		vim.keymap.set("n", "<leader>tn", builtin.next({ jump = true, skip_group = true }, {})
+		vim.keymap.set("n", "<leader>tp", builtin.previous({ jump = true, skip_group = true }, {})
+		vim.keymap.set("n", "<leader>tt", builtin.toggle, {})
+		vim.keymap.set("n", "<leader>tt", builtin.toggle, {})
+	end,
 }
