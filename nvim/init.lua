41291e8d537bdc96d71ec88a652a3e13be21e725
Judah Fuller <judah@judahfuller.com>
2025-07-03 15:08:02 +0100

    Add more neovim plugins

--- nvim/init.lua
+++ nvim/init.lua
@@ -9,3 +9,8 @@ vim.opt.relativenumber = true
 -- Force transparency
 vim.api.nvim_set_hl(0, "Normal", { bg = "NONE" })
 vim.api.nvim_set_hl(0, "NormalFloat", { bg = "NONE" })
+
+-- Set tab size
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
