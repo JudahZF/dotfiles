e7e023a25c4ab0e78e557a13743c4cec11fa919b
Judah Fuller <judah@judahfuller.com>
2025-10-13 10:32:10 +0100

    Ensure proper nixos module

--- nix/flake.nix
+++ nix/flake.nix
@@ -54,6 +54,7 @@
         gale = libx.mkDarwin { hostname = "gale"; };
       };
       nixosConfigurations.popper = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
         specialArgs = {
           inherit inputs outputs;
           name = "popper";
