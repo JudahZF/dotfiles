8b7617f2c32b6d7fb393d4003e3feb550188348c
Judah Fuller <judah@judahfuller.com>
2025-10-27 17:28:41 +0000

    Use different home for nix

---
+++ nix/home/judahf.nix
@@ -0,0 +1,21 @@
+{ dotfiles, pkgs, ... }: {
+  imports = [ ./cli ./desktop ];
+  home = {
+    stateVersion = "24.05";
+  };
+
+  programs = {
+    ghostty = {
+      enable = true;
+      settings = { background-opacity = 0.8; };
+    };
+    zsh = {
+      enable = true;
+      enableCompletion = true;
+      initContent = if pkgs.stdenv.isDarwin then
+        builtins.readFile "${dotfiles}/zsh/macos/zshrc"
+      else
+        builtins.readFile "${dotfiles}/zsh/linux/zshrc";
+    };
+  };
+}
