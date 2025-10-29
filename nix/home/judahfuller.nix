73ca3d36d9d0759b2081fd3fb58aa28c46d87713
Judah Fuller <judah@judahfuller.com>
2025-10-29 14:13:20 +0000

    Setup git

--- nix/home/judahfuller.nix
+++ nix/home/judahfuller.nix
@@ -30,29 +30,6 @@
       enable = true;
       settings = { background-opacity = 0.8; };
     };
-    git = {
-      enable = true;
-      difftastic = {
-        options.color = "always";
-        enableAsDifftool = true;
-        enable = true;
-      };
-      extraConfig = if pkgs.stdenv.isDarwin then {
-        gpg."ssh".program =
-          "/Applications/1Password.app/Contents/MacOS/op-ssh-sign";
-      } else
-        { };
-      lfs = { enable = true; };
-      ignores = [ ".env" ];
-      signing = {
-        format = "ssh";
-        key =
-          "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKg+m/SsrTx6+3t00tabRdDLms4jYrxGwlh8gG7ZkIsO";
-        signByDefault = true;
-      };
-      userEmail = "judah@judahfuller.com";
-      userName = "Judah Fuller";
-    };
     zsh = {
       enable = true;
       enableCompletion = true;
