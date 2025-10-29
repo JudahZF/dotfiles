7e212dca2e997fe2e2b079d5d1e34da85cef98eb
Judah Fuller <judah@judahfuller.com>
2025-10-29 10:59:56 +0000

    Move walker import

--- nix/home/desktop/walker.nix
+++ nix/home/desktop/walker.nix
@@ -1,12 +1,9 @@
-{ inputs, ... }: {
-  imports = [inputs.walker.homeManagerModules.default];
-
+{ ... }: {
   programs.walker = {
     enable = true;
     runAsService = true;
 
     config = {
-      placeholders."default" = { input = "Search"; list = "Example"; };
       keybinds.quick_activate = ["1" "2" "3" "4" "5" "6" "7" "8"];
     };
   };
