c41bbbb6364864ac8b857a62ad5f6b1445c4f02a
Judah Fuller <judah@judahfuller.com>
2025-10-29 14:24:56 +0000

    Make rebuild script universal

--- nix/_internal_rebuild.sh
+++ nix/_internal_rebuild.sh
@@ -1,1 +1,15 @@
-sudo --preserve-env=HOME darwin-rebuild switch --flake /Users/judahfuller/dotfiles/nix#gale --show-trace --max-jobs auto --cores 0
+#!/bin/bash
+
+# Detect OS and run appropriate rebuild command
+OS=$(uname -s)
+
+if [[ "$OS" == "Darwin" ]]; then
+  # macOS
+  sudo --preserve-env=HOME darwin-rebuild switch --flake /Users/judahfuller/dotfiles/nix --show-trace --max-jobs auto --cores 0
+elif [[ "$OS" == "Linux" ]]; then
+  # Linux
+  sudo --preserve-env=HOME nixos-rebuild switch --flake /Users/judahfuller/dotfiles/nix --show-trace --max-jobs auto --cores 0
+else
+  echo "Unsupported operating system: $OS"
+  exit 1
+fi
