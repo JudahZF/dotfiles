a43db1925be705127e10b46c59a6e1e55ad833d2
Judah Fuller <judah@judahfuller.com>
2025-10-29 10:52:21 +0000

    Upgrade walker

--- nix/home/desktop/walker.nix
+++ nix/home/desktop/walker.nix
@@ -1,1 +1,13 @@
-{ ... }: { services.walker = { enable = true; }; }
+{ inputs, ... }: {
+  imports = [inputs.walker.homeManagerModules.default];
+
+  programs.walker = {
+    enable = true;
+    runAsService = true;
+
+    config = {
+      placeholders."default" = { input = "Search"; list = "Example"; };
+      keybinds.quick_activate = ["1" "2" "3" "4" "5" "6" "7" "8"];
+    };
+  };
+}
