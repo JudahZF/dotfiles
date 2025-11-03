fd84cf9f19aa3f4a18a5947697998e50e696c7b1
Judah Fuller <judah@judahfuller.com>
2025-11-03 11:24:23 +0000

    Update hyprland.nix

--- nix/hosts/nixos/popper/hyprland.nix
+++ nix/hosts/nixos/popper/hyprland.nix
@@ -2,8 +2,8 @@
   wayland.windowManager.hyprland = {
     settings = {
       monitor = [
-        "HDMI-A-3, 1920x1080@100, 0x0, 1"
-        "HDMI-A-2, 1920x1080@100, 1920x0, 1"
+        "HDMI-A-3, 1920x1080@75, 0x0, 1"
+        "HDMI-A-2, 1920x1080@75, 1920x0, 1"
       ];
     };
   };
