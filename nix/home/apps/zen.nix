b08d3d2178228545219ace5d006f876c89dd73d9
Judah Fuller <judah@judahfuller.com>
2025-11-18 15:06:11 +0000

    format

--- nix/home/apps/zen.nix
+++ nix/home/apps/zen.nix
@@ -2,64 +2,65 @@
   programs.zen-browser = {
     profiles = {
       "default" = {
-          containersForce = true;
-          containers = {
-            Personal = {
-              color = "blue";
-              icon = "fingerprint";
-              id = 1;
-            };
-            Work = {
-              color = "orange";
-              icon = "briefcase";
-              id = 2;
-            };
-            "Work Admin" = {
-              color = "red";
-              icon = "briefcase";
-              id = 3;
-            };
-            University = {
-              color = "purple";
-              icon = "briefcase";
-              id = 4;
-            };
+        containersForce = true;
+        containers = {
+          Personal = {
+            color = "blue";
+            icon = "fingerprint";
+            id = 1;
           };
-          extensions = with inputs.firefox-addons.packages.${pkgs.system}; [
-            darkreader
-            enhancer-for-nebula
-            enhancer-for-youtube
-            facebook-container
-            languagetool
-            onepassword-password-manager
-            return-youtube-dislikes
-            ublock-origin
-          ];
-          spacesForce = true;
-          spaces = let
-            containers = config.programs.zen-browser.profiles."default".containers;
-          in {
-            "Personal" = {
-              id = "c6de089c-410d-4206-961d-ab11f988d40a";
-              container = containers."Personal".id;
-              position = 1000;
-            };
-            "Projects" = {
-              id = "845d81a9-9c90-4792-8fe1-59eb53dc9a1c";
-              container = containers."Personal".id;
-              position = 2000;
-            };
-            "Work" = {
-              id = "cdd10fab-4fc5-494b-9041-325e5759195b";
-              container = containers."Work".id;
-              position = 3000;
-            };
-            "University" = {
-              id = "2d89feb2-c9f9-408e-9997-d13529817b22";
-              container = containers."University".id;
-              position = 4000;
-            };
+          Work = {
+            color = "orange";
+            icon = "briefcase";
+            id = 2;
           };
+          "Work Admin" = {
+            color = "red";
+            icon = "briefcase";
+            id = 3;
+          };
+          University = {
+            color = "purple";
+            icon = "briefcase";
+            id = 4;
+          };
+        };
+        extensions = with inputs.firefox-addons.packages.${pkgs.system}; [
+          darkreader
+          enhancer-for-nebula
+          enhancer-for-youtube
+          facebook-container
+          languagetool
+          onepassword-password-manager
+          return-youtube-dislikes
+          ublock-origin
+        ];
+        spacesForce = true;
+        spaces = let
+          containers =
+            config.programs.zen-browser.profiles."default".containers;
+        in {
+          "Personal" = {
+            id = "c6de089c-410d-4206-961d-ab11f988d40a";
+            container = containers."Personal".id;
+            position = 1000;
+          };
+          "Projects" = {
+            id = "845d81a9-9c90-4792-8fe1-59eb53dc9a1c";
+            container = containers."Personal".id;
+            position = 2000;
+          };
+          "Work" = {
+            id = "cdd10fab-4fc5-494b-9041-325e5759195b";
+            container = containers."Work".id;
+            position = 3000;
+          };
+          "University" = {
+            id = "2d89feb2-c9f9-408e-9997-d13529817b22";
+            container = containers."University".id;
+            position = 4000;
+          };
+        };
       };
     };
     policies = {
@@ -87,14 +88,12 @@
       PasswordManagerEnabled = false;
       SearchEngines = {
         Default = "DuckDuckGo";
-        Custom = [
-          {
-            Name = "DuckDuckGo";
-            Template = "https://duckduckgo.com/?q={searchTerms}";
-            IconURL = "https://duckduckgo.com/favicon.ico";
-            IsDefault = true;
-          }
-        ];
+        Custom = [{
+          Name = "DuckDuckGo";
+          Template = "https://duckduckgo.com/?q={searchTerms}";
+          IconURL = "https://duckduckgo.com/favicon.ico";
+          IsDefault = true;
+        }];
       };
       SkipTermsOfUse = true;
       TranslateEnabled = true;
--- nix/hosts/common/dev-packages.nix
+++ nix/hosts/common/dev-packages.nix
@@ -1,48 +1,50 @@
-{ inputs, pkgs, pkgs-unstable ? null, system ? "aarch64-darwin", ... }: 
+{ inputs, pkgs, pkgs-unstable ? null, system ? "aarch64-darwin", ... }:
 let
-  unstable = if pkgs-unstable != null then pkgs-unstable else import inputs.nixpkgs-unstable {
-    inherit system;
-    config = {
-      allowUnfree = true;
-      allowUnfreePredicate = pkg: builtins.elem (inputs.nixpkgs-unstable.lib.getName pkg) [
-        "cursor"
-        "code-cursor"
-      ];
+  unstable = if pkgs-unstable != null then
+    pkgs-unstable
+  else
+    import inputs.nixpkgs-unstable {
+      inherit system;
+      config = {
+        allowUnfree = true;
+        allowUnfreePredicate = pkg:
+          builtins.elem (inputs.nixpkgs-unstable.lib.getName pkg) [
+            "cursor"
+            "code-cursor"
+          ];
+      };
     };
-  };
 in {
-   nixpkgs.config.allowUnfree = true;
-   nixpkgs.config.allowUnfreePredicate = pkg: builtins.elem (pkgs.lib.getName pkg) [
-     "cursor"
-     "code-cursor"
-   ];
-   environment.systemPackages = with pkgs; [
-      ## stable
-      bootdev-cli
-      bun
-      cmake
-      unstable.code-cursor
-     ghidra
-     go
-     jetbrains.datagrip
-     jq
-     lazygit
-     libpq
-     lua5_1
-     luarocks-nix
-     nixfmt
-     nixfmt-rfc-style
-     nodejs
-      ollama
-      unstable.opencode
-      phpactor
-      pnpm
-      unstable.prettier
-     protobuf
-     postman
-     (python313.withPackages (ps: with ps; [ pip requests mcp ]))
-      ruff
-      watchman
-      unstable.zed-editor
-   ];
+  nixpkgs.config.allowUnfree = true;
+  nixpkgs.config.allowUnfreePredicate = pkg:
+    builtins.elem (pkgs.lib.getName pkg) [ "cursor" "code-cursor" ];
+  environment.systemPackages = with pkgs; [
+    ## stable
+    bootdev-cli
+    bun
+    cmake
+    unstable.code-cursor
+    ghidra
+    go
+    jetbrains.datagrip
+    jq
+    lazygit
+    libpq
+    lua5_1
+    luarocks-nix
+    nixfmt
+    nixfmt-rfc-style
+    nodejs
+    ollama
+    unstable.opencode
+    phpactor
+    pnpm
+    unstable.prettier
+    protobuf
+    postman
+    (python313.withPackages (ps: with ps; [ pip requests mcp ]))
+    ruff
+    watchman
+    unstable.zed-editor
+  ];
 }
