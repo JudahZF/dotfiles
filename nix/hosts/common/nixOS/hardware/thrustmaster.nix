9dbb58a73b47c696bdd24fc8501b804c730b9326
Judah Fuller <judah@judahfuller.com>
2026-01-30 15:58:56 +0000

    Various changes

--- nix/hosts/common/nixOS/hardware/thrustmaster.nix
+++ nix/hosts/common/nixOS/hardware/thrustmaster.nix
@@ -5,7 +5,7 @@
   boot.blacklistedKernelModules = [ "hid-thrustmaster" ];
   boot.extraModulePackages = [ config.boot.kernelPackages.hid-tmff2 ];
   boot.kernelModules = [ "hid-tmff2" ];
-  boot.extraModprobeConfig = "options hid-tmff2 enable_unknow_wheels=1\n";
+  # boot.extraModprobeConfig = "options hid-tmff2\n";
 
   # Udev rules for Thrustmaster wheels (Vendor ID 044f)
   # Grants input group access for non-root usage
