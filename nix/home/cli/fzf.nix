3e12a74691a501c9b49d4014ee0c3d7084c2fab5
Judah Fuller <judah@judahfuller.com>
2026-02-02 12:33:17 +0000

    Add CLI tool configs (atuin, bat, direnv, eza, ripgrep) and updates

--- nix/home/cli/fzf.nix
+++ nix/home/cli/fzf.nix
@@ -2,5 +2,14 @@
   programs.fzf = {
     enable = true;
     enableZshIntegration = true;
+    defaultCommand = "fd --type f --hidden --follow --exclude .git";
+    defaultOptions = [
+      "--height=40%"
+      "--layout=reverse"
+      "--border"
+      "--preview='bat --style=numbers --color=always --line-range :500 {}'"
+    ];
+    fileWidgetCommand = "fd --type f --hidden --follow --exclude .git";
+    changeDirWidgetCommand = "fd --type d --hidden --follow --exclude .git";
   };
 }
