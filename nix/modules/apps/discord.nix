17c389e13708bc2cc28bb01c387c55dee7324647
Judah Fuller <judah@judahfuller.com>
2026-02-04 11:48:54 +0000

    Add yazi file manager with image preview support

--- nix/modules/apps/discord.nix
+++ nix/modules/apps/discord.nix
@@ -1,3 +1,5 @@
-{ pkgs, ... }: {
+{ pkgs, lib, ... }:
+# Discord not available on aarch64-linux
+lib.mkIf (pkgs.stdenv.isDarwin || pkgs.stdenv.hostPlatform.isx86_64) {
   environment.systemPackages = with pkgs; [ discord ];
 }
--- nix/modules/system/nixos/bootloader.nix
+++ nix/modules/system/nixos/bootloader.nix
@@ -1,5 +1,6 @@
 { pkgs, lib, ... }:
-lib.mkIf pkgs.stdenv.isLinux {
+# Only enable systemd-boot on x86_64 Linux (RPi5 uses its own bootloader via nixos-hardware)
+lib.mkIf (pkgs.stdenv.isLinux && pkgs.stdenv.hostPlatform.isx86_64) {
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 }
